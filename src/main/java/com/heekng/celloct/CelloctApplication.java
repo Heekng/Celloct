41360bae15f677a9786ab96418a7e1720574e3b6
heekng <rhkd1769@gmail.com>
2022-03-20 02:54:04 +0900

    initial commit

---
+++ src/main/java/com/heekng/celloct/CelloctApplication.java
@@ -0,0 +1,13 @@
+package com.heekng.celloct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelloctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelloctApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heekng/celloct/CelloctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heekng.celloct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelloctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
