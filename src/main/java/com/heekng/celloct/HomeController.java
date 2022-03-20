a547174ca1b8610c984584651944cd76516d9d37
heekng <rhkd1769@gmail.com>
2022-03-20 18:33:47 +0900

    test name

--- src/main/java/com/heekng/celloct/HomeController.java
+++ src/main/java/com/heekng/celloct/HomeController.java
@@ -4,14 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Slf4j
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("name", "heekng");
+    public String home(@RequestParam("name") String name, Model model) {
+        if (name != null) {
+            model.addAttribute("name", name);
+        }
         return "home";
     }
 }
