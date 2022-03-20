40133e93b2354a495a1533f367d30e33b5f95176
heekng <rhkd1769@gmail.com>
2022-03-20 16:48:21 +0900

    add hello home

---
+++ src/main/java/com/heekng/celloct/HomeController.java
@@ -0,0 +1,17 @@
+package com.heekng.celloct;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("name", "heekng");
+        return "home";
+    }
+}
