542e580b4890034dda545c683d0124386c39e79e
heekng <rhkd1769@gmail.com>
2022-04-05 23:26:14 +0900

    Member entity builder 패턴 적용

--- src/main/java/com/heekng/celloct/entity/Member.java
+++ src/main/java/com/heekng/celloct/entity/Member.java
@@ -1,6 +1,9 @@
 package com.heekng.celloct.entity;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -8,6 +11,7 @@ import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id
@@ -27,4 +31,14 @@ public class Member {
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<JoinRequest> joinRequests = new ArrayList<>();
+
+    @Builder
+    public Member(String email, String password, String name, List<Authority> authorities, List<Shop> shops, List<JoinRequest> joinRequests) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.authorities = authorities;
+        this.shops = shops;
+        this.joinRequests = joinRequests;
+    }
 }
