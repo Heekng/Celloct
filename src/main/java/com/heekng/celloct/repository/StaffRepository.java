c4ee28bc9891fb3e35e03ff181bb0fb7abbad69a
heekng <rhkd1769@gmail.com>
2022-04-08 00:16:23 +0900

    findByMemberIdAndShopId 추가

--- src/main/java/com/heekng/celloct/repository/StaffRepository.java
+++ src/main/java/com/heekng/celloct/repository/StaffRepository.java
@@ -3,5 +3,9 @@ package com.heekng.celloct.repository;
 import com.heekng.celloct.entity.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StaffRepository extends JpaRepository<Staff, Long> {
+
+    List<Staff> findByMemberIdAndShopId(Long memberId, Long shopId);
 }
