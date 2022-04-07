e33371479e334dfbd07579086fec0625bda67d1a
heekng <heekng@epinet.kr>
2022-04-07 16:37:20 +0900

    staffRepositoryTest 작성중

---
+++ src/test/java/com/heekng/celloct/repository/StaffRepositoryTest.java
@@ -0,0 +1,91 @@
+package com.heekng.celloct.repository;
+
+import com.heekng.celloct.entity.Member;
+import com.heekng.celloct.entity.Shop;
+import com.heekng.celloct.entity.Staff;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class StaffRepositoryTest {
+
+    @Autowired
+    StaffRepository staffRepository;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    ShopRepository shopRepository;
+
+    @Test
+    @DisplayName("staff 저장 테스트")
+    void save() throws Exception {
+        //given
+        Member shopMember = Member.builder()
+                .name("shopMember")
+                .build();
+        memberRepository.save(shopMember);
+
+        Member staffMember = Member.builder()
+                .name("staffMember")
+                .build();
+        memberRepository.save(staffMember);
+
+        Shop shop = Shop.builder()
+                .member(shopMember)
+                .build();
+        shopRepository.save(shop);
+
+        Staff staff = Staff.builder()
+                .shop(shop)
+                .member(staffMember)
+                .build();
+        staffRepository.save(staff);
+        //when
+        Staff findStaff = staffRepository.findById(staff.getId()).get();
+
+        //then
+        assertThat(staff.getMember()).isEqualTo(findStaff.getMember());
+    }
+
+    @Test
+    void delete() throws Exception {
+        //given
+        Member shopMember = Member.builder()
+                .name("shopMember")
+                .build();
+        memberRepository.save(shopMember);
+
+        Member staffMember = Member.builder()
+                .name("staffMember")
+                .build();
+        memberRepository.save(staffMember);
+
+        Shop shop = Shop.builder()
+                .member(shopMember)
+                .build();
+        shopRepository.save(shop);
+
+        Staff staff = Staff.builder()
+                .shop(shop)
+                .member(staffMember)
+                .build();
+        staffRepository.save(staff);
+        //when
+        staffRepository.delete(staff);
+        Staff findStaff = staffRepository.findById(staff.getId()).get();
+        //then
+        assertThat(findStaff).isNull();
+
+    }
+
+}
