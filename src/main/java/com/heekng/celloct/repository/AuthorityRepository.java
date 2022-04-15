f248fc11bf76e4439ebb432f5384e63530d761e8
heekng <rhkd1769@gmail.com>
2022-04-16 01:42:16 +0900

    단일 role 정책으로 수정

--- src/main/java/com/heekng/celloct/repository/AuthorityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.heekng.celloct.repository;
-
-import com.heekng.celloct.entity.Authority;
-import com.heekng.celloct.entity.AuthorityId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, AuthorityId> {
-}
