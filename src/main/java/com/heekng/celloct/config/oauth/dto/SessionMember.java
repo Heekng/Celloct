1a76d8a57fd39479b4d34d5b4b5f2100028e4b5b
heekng <rhkd1769@gmail.com>
2022-04-21 00:13:53 +0900

    oauth 로그인시 id값도 저장하도록 수정

--- src/main/java/com/heekng/celloct/config/oauth/dto/SessionMember.java
+++ src/main/java/com/heekng/celloct/config/oauth/dto/SessionMember.java
@@ -8,11 +8,13 @@ import java.io.Serializable;
 @Getter
 public class SessionMember implements Serializable {
 
+    private Long id;
     private String name;
     private String email;
     private String picture;
 
     public SessionMember(Member member) {
+        this.id = member.getId();
         this.name = member.getName();
         this.email = member.getEmail();
         this.picture = member.getPicture();
