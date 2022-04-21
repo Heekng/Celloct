05aef5b4f13b20a0788e137c8b8252d830163507
heekng <rhkd1769@gmail.com>
2022-04-21 20:57:09 +0900

    shop 생성 서비스 수정으로 인한 테스트코드 수정

--- src/test/java/com/heekng/celloct/service/ShopServiceTest.java
+++ src/test/java/com/heekng/celloct/service/ShopServiceTest.java
@@ -4,6 +4,7 @@ import com.heekng.celloct.dto.ShopDto;
 import com.heekng.celloct.entity.Manager;
 import com.heekng.celloct.entity.Member;
 import com.heekng.celloct.entity.Shop;
+import com.heekng.celloct.repository.MemberRepository;
 import com.heekng.celloct.repository.ShopRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -24,19 +25,23 @@ class ShopServiceTest {
     @Autowired
     ShopRepository shopRepository;
     @Autowired
-    MemberService memberService;
-    @Autowired
-    EntityManager em;
+    MemberRepository memberRepository;
 
     @Test
     void make() throws Exception {
         //given
+        Member member = Member.builder()
+                .name("member1")
+                .build();
+        memberRepository.save(member);
+
         ShopDto.createRequest createRequest = ShopDto.createRequest.builder()
                 .name("shop1")
                 .phone("010-1234-1234")
+                .managerName("")
                 .build();
         //when
-        Long shopId = shopService.makeShop(createRequest);
+        Long shopId = shopService.makeShop(createRequest, member.getId());
         Shop shop = shopRepository.findById(shopId).get();
 
         //then
