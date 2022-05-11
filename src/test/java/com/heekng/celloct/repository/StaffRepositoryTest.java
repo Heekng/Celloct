3221be44c8a53c43bcee326c3aa2596e95549c4b
heekng <rhkd1769@gmail.com>
2022-05-11 21:30:17 +0900

    StaffRepository 테스트 코드 수정 및 추가

--- src/test/java/com/heekng/celloct/repository/StaffRepositoryTest.java
+++ src/test/java/com/heekng/celloct/repository/StaffRepositoryTest.java
@@ -10,8 +10,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -21,6 +23,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 class StaffRepositoryTest {
 
+    @Autowired
+    EntityManager em;
     @Autowired
     StaffRepository staffRepository;
     @Autowired
@@ -34,11 +38,13 @@ class StaffRepositoryTest {
         //given
         Member shopMember = Member.builder()
                 .name("shopMember")
+                .email("shopMember@gmail.com")
                 .build();
         memberRepository.save(shopMember);
 
         Member staffMember = Member.builder()
                 .name("staffMember")
+                .email("staffMember@gmail.com")
                 .build();
         memberRepository.save(staffMember);
 
@@ -50,13 +56,18 @@ class StaffRepositoryTest {
         Staff staff = Staff.builder()
                 .shop(shop)
                 .member(staffMember)
+                .name("staff")
                 .build();
         staffRepository.save(staff);
+
+        em.flush();
+        em.clear();
+
         //when
         Staff findStaff = staffRepository.findById(staff.getId()).get();
 
         //then
-        assertThat(staff.getMember()).isEqualTo(findStaff.getMember());
+        assertThat(staff.getId()).isEqualTo(findStaff.getId());
     }
 
     @Test
@@ -64,11 +75,13 @@ class StaffRepositoryTest {
         //given
         Member shopMember = Member.builder()
                 .name("shopMember")
+                .email("shopMember@gmail.com")
                 .build();
         memberRepository.save(shopMember);
 
         Member staffMember = Member.builder()
                 .name("staffMember")
+                .email("staffMember@gmail.com")
                 .build();
         memberRepository.save(staffMember);
 
@@ -80,13 +93,240 @@ class StaffRepositoryTest {
         Staff staff = Staff.builder()
                 .shop(shop)
                 .member(staffMember)
+                .name("staff")
                 .build();
         staffRepository.save(staff);
+
+        em.flush();
+        em.clear();
+
         //when
-        staffRepository.delete(staff);
+        Staff findStaff = staffRepository.findById(staff.getId()).get();
+        staffRepository.delete(findStaff);
+        em.flush();
+        em.clear();
+
         Optional<Staff> staffOptional = staffRepository.findById(staff.getId());
         //then
         assertThat(staffOptional).isEmpty();
+    }
+
+    @Test
+    void findByMemberIdAndShopIdTest() throws Exception {
+        //given
+        Member shopMember = Member.builder()
+                .name("shopMember")
+                .email("shopMember@gmail.com")
+                .build();
+        memberRepository.save(shopMember);
+
+        Member staffMember = Member.builder()
+                .name("staffMember")
+                .email("staffMember@gmail.com")
+                .build();
+        memberRepository.save(staffMember);
+
+        Shop shop = Shop.builder()
+                .name("shop1")
+                .build();
+        shopRepository.save(shop);
+
+        Staff staff = Staff.builder()
+                .shop(shop)
+                .member(staffMember)
+                .name("staff")
+                .build();
+        staffRepository.save(staff);
+
+        em.flush();
+        em.clear();
+
+        //when
+        Optional<Staff> findStaff = staffRepository.findByMemberIdAndShopId(staffMember.getId(), shop.getId());
+
+        //then
+        assertThat(findStaff.get().getId()).isEqualTo(staff.getId());
+    }
+
+    @Test
+    void findByShopIdTest() throws Exception {
+        //given
+        Member shopMember = Member.builder()
+                .name("shopMember")
+                .email("shopMember@gmail.com")
+                .build();
+        memberRepository.save(shopMember);
+
+        Member staffMember = Member.builder()
+                .name("staffMember")
+                .email("staffMember@gmail.com")
+                .build();
+        memberRepository.save(staffMember);
+
+        Shop shop = Shop.builder()
+                .name("shop1")
+                .build();
+        shopRepository.save(shop);
+
+        Staff staff = Staff.builder()
+                .shop(shop)
+                .member(staffMember)
+                .name("staff")
+                .build();
+        staffRepository.save(staff);
+
+        em.flush();
+        em.clear();
+        //when
+        List<Staff> staffs = staffRepository.findByShopId(shop.getId());
+
+        //then
+        assertThat(staffs.get(0).getId()).isEqualTo(staff.getId());
+    }
+
+    @Test
+    void findByMemberIdTest() throws Exception {
+        //given
+        Member shopMember = Member.builder()
+                .name("shopMember")
+                .email("shopMember@gmail.com")
+                .build();
+        memberRepository.save(shopMember);
+
+        Member staffMember = Member.builder()
+                .name("staffMember")
+                .email("staffMember@gmail.com")
+                .build();
+        memberRepository.save(staffMember);
+
+        Shop shop = Shop.builder()
+                .name("shop1")
+                .build();
+        shopRepository.save(shop);
+
+        Staff staff = Staff.builder()
+                .shop(shop)
+                .member(staffMember)
+                .name("staff")
+                .build();
+        staffRepository.save(staff);
+
+        em.flush();
+        em.clear();
+        //when
+        List<Staff> staffs = staffRepository.findByMemberId(staffMember.getId());
+
+        //then
+        assertThat(staffs.get(0).getId()).isEqualTo(staff.getId());
+    }
+    
+    @Test
+    void findWithMemberByIdTest() throws Exception {
+        //given
+        Member shopMember = Member.builder()
+                .name("shopMember")
+                .email("shopMember@gmail.com")
+                .build();
+        memberRepository.save(shopMember);
+
+        Member staffMember = Member.builder()
+                .name("staffMember")
+                .email("staffMember@gmail.com")
+                .build();
+        memberRepository.save(staffMember);
+
+        Shop shop = Shop.builder()
+                .name("shop1")
+                .build();
+        shopRepository.save(shop);
+
+        Staff staff = Staff.builder()
+                .shop(shop)
+                .member(staffMember)
+                .name("staff")
+                .build();
+        staffRepository.save(staff);
+
+        em.flush();
+        em.clear();
+        //when
+        Staff findStaff = staffRepository.findWithMemberById(staff.getId());
+        em.flush();
+        em.clear();
+
+        //then
+        assertThat(findStaff.getMember().getId()).isEqualTo(staffMember.getId());
+    }
+
+    @Test
+    void findByShopIdAndIdTest() throws Exception {
+        //given
+        Member shopMember = Member.builder()
+                .name("shopMember")
+                .email("shopMember@gmail.com")
+                .build();
+        memberRepository.save(shopMember);
+
+        Member staffMember = Member.builder()
+                .name("staffMember")
+                .email("staffMember@gmail.com")
+                .build();
+        memberRepository.save(staffMember);
+
+        Shop shop = Shop.builder()
+                .name("shop1")
+                .build();
+        shopRepository.save(shop);
+
+        Staff staff = Staff.builder()
+                .shop(shop)
+                .member(staffMember)
+                .name("staff")
+                .build();
+        staffRepository.save(staff);
+
+        em.flush();
+        em.clear();
+        //when
+        Staff findStaff = staffRepository.findByShopIdAndId(shop.getId(), staff.getId()).get();
+        //then
+        assertThat(findStaff.getId()).isEqualTo(staff.getId());
+    }
+
+    @Test
+    void findWithShopByMemberIdTest() throws Exception {
+        //given
+        Member shopMember = Member.builder()
+                .name("shopMember")
+                .email("shopMember@gmail.com")
+                .build();
+        memberRepository.save(shopMember);
+
+        Member staffMember = Member.builder()
+                .name("staffMember")
+                .email("staffMember@gmail.com")
+                .build();
+        memberRepository.save(staffMember);
+
+        Shop shop = Shop.builder()
+                .name("shop1")
+                .build();
+        shopRepository.save(shop);
+
+        Staff staff = Staff.builder()
+                .shop(shop)
+                .member(staffMember)
+                .name("staff")
+                .build();
+        staffRepository.save(staff);
+
+        em.flush();
+        em.clear();
+        //when
+        List<Staff> staffs = staffRepository.findWithShopByMemberId(staffMember.getId());
+
+        //then
+        assertThat(staffs.get(0).getId()).isEqualTo(staff.getId());
     }
 
 }
